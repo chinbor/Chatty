af2fe30ae2c0605a9dd2f20dbb264cbf6a80fc4a
chinbor Mn <chinbor.Mn@gmail.com>
2023-04-24 12:24:16 +0800

    feat: complete login page

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -2,45 +2,78 @@
 defineOptions({
   name: 'Login',
 })
-const router = useRouter()
-const appid = ref('')
-const secret = ref('')
+const appID = ref('')
+const secretKey = ref('')
+const userID = ref('')
 const isShowSelect = ref(false)
+const userStore = useUserObservable()
+const { t } = useI18n()
+const router = useRouter()
 
-function go() {
+function back() {
+  isShowSelect.value = false
+}
+
+async function go() {
   if (!isShowSelect.value) {
+    if (!appID.value) {
+      alert(`${t('input.appid_no_empty')}`)
+      return
+    }
+
+    if (!secretKey.value) {
+      alert(`${t('input.secret_no_empty')}`)
+      return
+    }
+
     isShowSelect.value = true
     return
   }
 
-  // TODO: 先走store的登录并将相关信息存储在localstorage中
+  if (!/^[a-zA-Z][a-zA-Z0-9_]{3,23}$/.test(userID.value)) {
+    alert(`${t('input.username_tip')}`)
+    return
+  }
+
+  await userStore.value.login({
+    userID: userID.value,
+    appID: Number(appID.value),
+    secretKey: secretKey.value,
+  })
+
   router.push('/')
 }
-
-const { t } = useI18n()
 </script>
 
 <template>
-  <div v-if="!isShowSelect" class="f-c-c gap-30px mt-100px">
+  <div v-show="!isShowSelect" class="f-c-c gap-30px pt-100px">
     <h1>{{ t('title1') }}</h1>
     <TheInput
-      v-model="appid"
+      v-model="appID"
       :placeholder="t('input.appid')"
       autocomplete="false"
       type="text"
     />
     <TheInput
-      v-model="secret"
+      v-model="secretKey"
       :placeholder="t('input.secret')"
       autocomplete="false"
       type="password"
       @keydown.enter="go"
     />
   </div>
-  <div v-else class="f-c-c gap-30px mt-100px">
+  <div v-show="isShowSelect" class="f-c-c gap-30px mt-100px">
     <h1>{{ t('title2') }}</h1>
-    fuck
+    <TheInput
+      v-model="userID"
+      :placeholder="t('input.username')"
+      autocomplete="false"
+      type="text"
+    />
   </div>
+  <button v-if="isShowSelect" btn mt-30px mr-10px @click="back">
+    {{ t('button.back') }}
+  </button>
   <button btn mt-30px @click="go">
     {{ t('button.go') }}
   </button>
