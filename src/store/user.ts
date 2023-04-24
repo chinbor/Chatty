b037ec5a0e089c1b64fd3926b6bf176b8f84a3f2
chinbor Mn <chinbor.Mn@gmail.com>
2023-04-24 12:26:42 +0800

    feat: use mobx to store data

---
+++ src/store/user.ts
@@ -0,0 +1,67 @@
+import { createGlobalObservable, useLocalObservable } from 'mobx-vue-lite'
+import type { ChatSDK } from 'tim-js-sdk'
+import { createTim } from '~/tim'
+import { USER_INFO } from '~/constants'
+
+export interface LoginParams {
+  userID: string
+  appID: number
+  secretKey: string
+}
+
+interface UserObservable {
+  tim: ChatSDK | null
+  login: (params: LoginParams) => Promise<any>
+  reset: () => void
+  logout: () => Promise<void>
+}
+
+export const useUserObservable = createGlobalObservable(() => {
+  return useLocalObservable<UserObservable>(() => ({
+    tim: null,
+    async login(params) {
+      try {
+        const { userID, appID, secretKey } = params
+        const { tim, userSig } = createTim(userID, appID, secretKey)
+
+        this.tim = tim
+
+        const result = await tim.login({
+          userID,
+          userSig,
+        })
+
+        const userInfo = localStorage.getItem(USER_INFO)
+
+        if (!userInfo) {
+          localStorage.setItem(USER_INFO, JSON.stringify({
+            userID,
+            appID,
+            secretKey,
+          }))
+        }
+
+        return result
+      }
+      catch (err) {
+        console.error(err)
+      }
+    },
+    async logout() {
+      const router = useRouter()
+
+      if (!this.tim)
+        return
+
+      await this.tim.logout()
+
+      this.reset()
+
+      router.push('/login')
+    },
+    reset() {
+      this.tim = null
+      localStorage.removeItem('userInfo')
+    },
+  }))
+})
